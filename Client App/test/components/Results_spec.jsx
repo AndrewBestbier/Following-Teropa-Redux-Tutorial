9d1868a275bc5ce231d4b772387deac2b6d089e3
Andrew Bestbier <andrew.bestbier@gmail.com>
2015-10-27 17:51:16 +0200

    Added routing, Results and some basic results tests

---
+++ Client App/test/components/Results_spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react/addons';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+
+describe('results', () => {
+  it('test', () => {
+    expect(1).to.equal(1);
+  })
+})
